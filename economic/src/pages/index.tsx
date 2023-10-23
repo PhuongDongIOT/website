8d0444bc9b7fc0b73970b25a19033b24a5a8ad6c
DongPP <phongdongiot@gmai.com>
2023-10-23 14:32:56 +0700

    initialization project

---
+++ economic/src/pages/index.tsx
@@ -0,0 +1,11 @@
+const Home = ({ }) => {  
+    return (
+      <>
+        <section className="flex flex-col items-center gap-y-5">
+            <p>No results found.</p>
+        </section>
+      </>
+    );
+  };
+  
+  export default Home;
---
+++ economic/src/pages/results/index.tsx
@@ -0,0 +1,11 @@
+const Results = ({ }) => {  
+    return (
+      <>
+        <section className="flex flex-col items-center gap-y-5">
+            <p>No results found.</p>
+        </section>
+      </>
+    );
+  };
+  
+  export default Results;
