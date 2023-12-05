f737943a978317aa6b4cdddb9044aa84c35f5bde
dongpp <phuongdongiot@gmail.com>
2023-12-05 23:27:39 +0700

    feat: add peerjs

--- economic-ad/src/components/calls/main/components/RoomComponent.tsx
+++ economic-ad/src/components/calls/main/components/RoomComponent.tsx
@@ -3,54 +3,81 @@ import { Peer } from "peerjs";
 import { mainConfig } from '~configs/main.config';
 
 
-const VideoRoom: React.FC = () => {
+const VideoRoom = () => {
     if(mainConfig.isDevEnv) {
-
-      const videoRef = useRef<HTMLInputElement>(null);
+      const [peerId, setPeerId] = useState('');
+      const [remotePeerIdValue, setRemotePeerIdValue] = useState('');
+      const remoteVideoRef = useRef<HTMLVideoElement & any >(null);
+      const currentUserVideoRef = useRef<HTMLVideoElement& any>(null);
+      const peerInstance = useRef<any>(null);
 
       useEffect(() => {
-        import('peerjs').then(({ default: Peer }) => {
           
-      const peer = new Peer({
-        host: "localhost",
-        port: 9001,
-        path: "",
-      });
-      
-      const conn = peer.connect("9d629fbd-a608-401a-be64-36bee60f9308");
-      conn.on("open", () => {
-        conn.send("Hello World!");
-      });
-      conn.on("data", (data) => {
-        console.log("Received data", data);
-      });
-      const getVideo = () => {
-        if(navigator.mediaDevices) {
-          navigator.mediaDevices.getUserMedia({ video: true, audio: true })          
-          .then((stream: any) => {
-            const call = peer.call("another-peers-id", stream);
-            call.on("stream", (remoteStream) => {              
-              let video = videoRef.current;
-              if(video) {
-                video.srcObject = stream;
-                video.play();
-                console.log('hihi')
+        const peer = new Peer();
+        
+        // const conn = peer.connect("9d629fbd-a608-401a-be64-36bee60f9308");
+        peer.on("open", (id) => {
+          setPeerId(id)
+        });
+        peer.on('call', (call) => {
+          var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+    
+          getUserMedia({ video: true, audio: true }, (mediaStream: any | null) => {
+            let devcurrentUserVideoRef = currentUserVideoRef.current;
+            if(devcurrentUserVideoRef) {
+              devcurrentUserVideoRef.srcObject = mediaStream;
+              devcurrentUserVideoRef.play();
+            }
+            call.answer(mediaStream)
+            call.on('stream', function(remoteStream) {
+              let devRemoteVideoRef = remoteVideoRef.current;
+              if(devRemoteVideoRef) {
+                devRemoteVideoRef.srcObject = remoteStream
+                devRemoteVideoRef.play();
               }
             });
-          })
-          .catch((err: any) => {
-            console.error("Failed to get local stream", err);
           });
+        })
+    
+        peerInstance.current = peer;
+      }, []);
 
-        }
-      };
-        getVideo();
-      });
-      }, [videoRef]);
+      
+      const call = (remotePeerId) => {
+        var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+    
+        getUserMedia({ video: true, audio: true }, (mediaStream: any | null) => {
+    
+
+          let devcurrentUserVideoRef = currentUserVideoRef.current;
+          if(devcurrentUserVideoRef) {
+            devcurrentUserVideoRef.srcObject = mediaStream;
+            devcurrentUserVideoRef.play();
+          }
+    
+          const call = peerInstance.current.call(remotePeerId, mediaStream)
+    
+          call.on('stream', (remoteStream: any | null) => {
+            let devRemoteVideoRef = remoteVideoRef.current;
+            if(devRemoteVideoRef) {
+              devRemoteVideoRef.srcObject = remoteStream
+              devRemoteVideoRef.play();
+            }
+          });
+        });
+      }
 
       return(
         <div>
-          <video ref={videoRef}/>
+          <h1>Current user id is {peerId}</h1>
+          <input type="text" value={remotePeerIdValue} onChange={(e: React.ChangeEvent<HTMLInputElement & any>) => setRemotePeerIdValue(e.target.value)} />
+          <button onClick={() => call(remotePeerIdValue)}>Call</button>
+          <div>
+            <video ref={currentUserVideoRef} />
+          </div>
+          <div>
+            <video ref={remoteVideoRef} />
+          </div>
         </div>
       )
     }
@@ -82,7 +109,7 @@ const VideoRoom: React.FC = () => {
           .then((stream: any) => {
             call.answer(stream); // Answer the call with an A/V stream.
             call.on("stream", (remoteStream) => {
-              let video = videoCallRef.current;
+              let video: any = videoCallRef.current;
               if(video) {
                 video.srcObject = remoteStream;
                 video.play();
